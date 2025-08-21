d6f88587aa51966338514d01183024bcf5caf72b
Nikhil Sharma <63258571+izuxhd@users.noreply.github.com>
2025-08-22 01:05:49 +0530

    Commit

---
+++ src/main/java/com/codewithapp/blog/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.codewithapp.blog.services;
+
+
+
+import java.util.List;
+
+import com.codewithapp.blog.payloads.CategoryDto;
+
+public interface CategoryService {
+	
+	// create 
+	CategoryDto createCategory(CategoryDto categoryDto);
+	
+	// update
+	CategoryDto updateCategory(CategoryDto categoryDto, Integer categoryId);
+	
+	// delete
+	void deleteCategory(Integer categoryId);
+	
+	// get single
+	CategoryDto getCategory(Integer categoryId);
+	
+	// get all
+	List<CategoryDto> getCategories();
+
+}
